368fc03648b7ccb748d875f1fcc5a15e01bada99
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-01-26 14:05:03 +0200

    Added an example implementation script

---
+++ tests/generate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from util import stream, encode_image_to_base64
+
+MODEL_PATH = 'wangqixun/YamerMIX_v8'
+IMAGE_PATH = '../data/mypic.jpg'
+PROMPT = 'a man'
+NEGATIVE_PROMPT = 'nsfw'
+STYLE = 'Watercolor'
+NUM_STEPS = 30
+IDENTITYNET_STRENGTH_RATIO = 0.8
+ADAPTER_STRENGTH_RATIO = 0.8
+GUIDANCE_SCALE = 5
+SEED = 42
+
+
+if __name__ == '__main__':
+    payload = {
+        "input": {
+            "model": MODEL_PATH,
+            "face_image": encode_image_to_base64(IMAGE_PATH),
+            "pose_image": None,
+            "prompt":  PROMPT,
+            "negative_prompt": NEGATIVE_PROMPT,
+            "style_name": STYLE,
+            "num_steps": NUM_STEPS,
+            "identitynet_strength_ratio": IDENTITYNET_STRENGTH_RATIO,
+            "adapter_strength_ratio":  ADAPTER_STRENGTH_RATIO,
+            "guidance_scale": GUIDANCE_SCALE,
+            "seed": SEED
+        }
+    }
+
+    stream(payload)
