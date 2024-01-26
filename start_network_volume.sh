2927e5fd3f05d4e052035c47af961d70a0e83e48
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-01-26 17:26:59 +0200

    Bugfix for Docker start script on Network volumes

--- start_network_volume.sh
+++ start_network_volume.sh
@@ -7,6 +7,6 @@ ln -s /runpod-volume /workspace
 
 echo "Starting RunPod Handler"
 export PYTHONUNBUFFERED=1
-source /workspace/runpod-worker-instantid/venv/bin/activate
-cd /workspace/runpod-worker-instantid
+source /workspace/venv/bin/activate
+cd /workspace/runpod-worker-instantid/src
 python3 -u rp_handler.py
