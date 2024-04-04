e2125b63ab75058c1630c4f7a822150d2b0383e8
unrulpkk <129469691+unrulpkk@users.noreply.github.com>
2024-04-04 16:33:02 +0800

    Update download_checkpoints.py

--- src/download_checkpoints.py
+++ src/download_checkpoints.py
@@ -57,7 +57,7 @@ def get_instantid_pipeline():
         'torch_dtype': torch_dtype,
     }
 
-    pipeline = fetch_pretrained_model('stabilityai/stable-diffusion-xl-base-1.0', **args)
+    pipeline = fetch_pretrained_model('Justin-Choo/XXMix_9realisticSDXL', **args)
 
     return pipeline
 
